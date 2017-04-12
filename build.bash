74d45c4d3d38943f6085f04f395b81f7528c8ac8
ryanlovett <rylo@berkeley.edu>
2017-04-12 12:58:07 -0700

    Reorganize user structure.

--- build.bash
+++ build.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
-# Builds and pushes a given image to gcr.io + all nodes in current kubectl context
+
+# Builds and pushes a given image to gcr.io + all nodes in current kubectl
+# context
+
 set -e
 
 DOCKER_REPO=$(jq -r '.buildSettings.dockerRepo' 'docker-settings.json')
 CLUSTERS=$(jq -r '.clusters | join(" ")' 'docker-settings.json')
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 [ hub | proxy | base | user {user_image_type} ]"
+	echo "Usage: $0 [ base | {user_image_type} ]"
 	exit 1
 fi
 
@@ -29,21 +32,15 @@ IMAGE="$1"
 GIT_REV=$(git log -n 1 --pretty=format:%h -- ${IMAGE})
 TAG="${GIT_REV}"
 
-if [ "${IMAGE}" == "user" ]; then
-    USER_IMAGE_TYPE="${2}"
-    DOCKERFILE="Dockerfile.${USER_IMAGE_TYPE}"
-    IMAGE_SPEC="${DOCKER_REPO}/jupyterhub-k8s-${IMAGE}-${USER_IMAGE_TYPE}:${TAG}"
-else
-    DOCKERFILE="Dockerfile"
-    IMAGE_SPEC="${DOCKER_REPO}/jupyterhub-k8s-${IMAGE}:${TAG}"
+IMAGE_SPEC="${DOCKER_REPO}/jupyterhub-k8s-user-${2}:${TAG}"
+
+if [ ! -f ${IMAGE}/Dockerfile ]; then
+	echo "No such file: ${IMAGE}/Dockerfile"
+	exit 1
 fi
 
 cd ${IMAGE}
-if [ ! -f ${DOCKERFILE} ]; then
-	echo "No such file: ${IMAGE}/${DOCKERFILE}"
-	exit 1
-fi
-docker build -t ${IMAGE_SPEC} -f ${DOCKERFILE} .
+docker build -t ${IMAGE_SPEC} .
 ${DOCKER_PUSH} ${IMAGE_SPEC}
 
 echo "Pushed ${IMAGE_SPEC}"
