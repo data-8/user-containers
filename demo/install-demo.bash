3034acc73b37ca8139cbaf100413bd643600f210
Gunjan Baid <gunjan_baid@berkeley.edu>
2017-04-26 16:32:09 +0000

    Adding data8assets content to try site.

--- demo/install-demo.bash
+++ demo/install-demo.bash
@@ -12,3 +12,13 @@ git remote add -f origin https://github.com/data-8/textbook.git
 echo "notebooks/*" >> .git/info/sparse-checkout
 echo "index.ipynb" >> .git/info/sparse-checkout
 git checkout gh-pages
+
+# Sparse checkout of notebooks from data8assets.
+mkdir /home/jovyan/assignments
+cd /home/jovyan/assignments
+git init
+git config core.sparseCheckout true
+git remote add -f origin https://github.com/data-8/data8assets.git
+echo "materials/sp17/*" >> .git/info/sparse-checkout
+echo "lec/sp17/*" >> .git/info/sparse-checkout
+git checkout gh-pages
